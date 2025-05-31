ec1b35a933185ff4102dd071294b71ce9c9c9a14
Denisenko Vladimir <landroshop@gmail.com>
2025-06-01 00:16:09 +0300

    31/05

---
+++ src/main/java/model/Food.java
@@ -0,0 +1,32 @@
+package model;
+
+import model.constants.Discount;
+
+public abstract class Food implements Discountable {
+    protected int amount;
+    protected double price;
+    protected boolean isVegetarian;
+
+    public Food(int amount, double price, boolean isVegetarian) {
+        this.amount = amount;
+        this.price = price;
+        this.isVegetarian = isVegetarian;
+    }
+
+
+    public int getDiscount() {
+        return Discount.DEFAULT;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public double totalPrice() {
+        return amount * price;
+    }
+
+    public  double discountPrice() {
+        return totalPrice() * (100 - getDiscount()) / 100.0;
+    }
+}
